b363a121836bf89e38e005be08f0eb0ff8abea3d
Seonggwon Yoon <keyakoto@gmail.com>
2018-09-18 16:21:21 +0900

    update: get_contents api
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+var Sequelize = require('sequelize');
 var express = require('express');
+var models = require('../models/');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/v1/get_contents', function(req, res) {
+	models.contents.findAll().then(full => res.json(full))
 });
 
 module.exports = router;
