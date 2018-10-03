b6aec28b46f4f65a54beb6a372ca63a16e6dd362
Seonggwon Yoon <keyakoto@gmail.com>
2018-10-03 14:30:57 +0000

    add: find api
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ var models = require('../models/');
 var router = express.Router();
 
 router.get('/api/v1/get_contents', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
 	models.contents.findAll().then(full => res.json(full))
+});
+
+router.post('/api/v1/find', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	var search = req.body.search;
+	models.contents.findAll({
+		where: {title: {like: '%' + search + '%'}}
+	}).then(data => res.json(data));	
 });
 
 module.exports = router;
