12cbb438a3206c2c4701556ef4f81b4af63134fc
Seonggwon Yoon <keyakoto@gmail.com>
2018-10-03 14:29:59 +0000

    add: body-parser
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ models.sequelize.sync().then( () => {
 })
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 
