c59ff88e681ad934b928686abc704cbf8cd24173
Seonggwon Yoon <keyakoto@gmail.com>
2018-10-14 01:31:44 +0900

    add: github userid check api
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var Sequelize = require('sequelize');
 var express = require('express');
 var models = require('../models/');
 var router = express.Router();
+const request = require('request');
 
 router.get('/api/v1/get_contents', function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -14,6 +15,26 @@ router.post('/api/v1/find', function(req, res) {
 	models.contents.findAll({
 		where: {title: {like: '%' + search + '%'}}
 	}).then(data => res.json(data));	
+});
+
+router.post('/api/v1/usercheck', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	var userid = req.body.userid;
+
+	var options = {
+  		url: 'https://api.github.com/users/' + userid,
+ 		headers: {
+    		'User-Agent': 'yaboja'
+  		}
+	};
+ 
+	request(options, function (error, response, body) {
+		if (response.statusCode == 200) {
+			res.json({'check': true});
+		} else {
+			res.json({'check': false});
+		}
+	});
 });
 
 module.exports = router;
