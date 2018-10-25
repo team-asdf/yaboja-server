df831831a437a93cdf4ec36fd16b2eeb5946df79
Seonggwon Yoon <keyakoto@gmail.com>
2018-10-25 11:35:14 +0900

    add: langlist table
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

---
+++ models/langlist.js
@@ -0,0 +1,20 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('langlist', {
+    id: {
+      type: DataTypes.INTEGER(11).UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }, {
+  	timestamps: false
+  }, {
+    tableName: 'langlist'
+  });
+};
