5f9b807577a0cb3fcda761b99ff753fe144bd9bf
Seonggwon Yoon <keyakoto@gmail.com>
2018-11-24 22:50:03 +0900

    Add update userid by keyword api
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/api/v1/signup.js
+++ routes/api/v1/signup.js
@@ -6,11 +6,22 @@ const request = require('request');
 
 router.post('/', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    models.user.create({
+    models.users.create({
         userid: req.body.userid,
         extract_language: req.body.extract_language,
         keyword: req.body.keyword
     }).then(result => res.json({"check": true}))
+});
+
+router.post('/:userid', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    var condition = {where: {userid: req.params.userid}};
+    var values = {keyword: req.body.keyword};
+    models.users.update(values, condition).then(function(result) {
+        res.json({'update': true});
+    }).catch(function(err) {
+        res.json({'update': false});
+    });
 });
 
 module.exports = router;
