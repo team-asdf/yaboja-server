cf6ce508424ddf70f1759f8e0bdea6b7f64c40ed
Seonggwon Yoon <keyakoto@gmail.com>
2018-11-24 22:37:22 +0900

    Fix crossdomain issue
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/api/v1/checker.js
+++ routes/api/v1/checker.js
@@ -6,6 +6,9 @@ const request = require('request');
 
 router.post('/', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
     var userid = req.body.userid;
 
     var options = {
