6f3e8e5fac3c0676808e7e5d71c5004c61721e19
Seonggwon Yoon <keyakoto@gmail.com>
2018-11-25 20:27:29 +0900

    Add to archives reading articles
    Signed-off-by: Seonggwon Yoon <keyakoto@gmail.com>

--- routes/api/v1/updater.js
+++ routes/api/v1/updater.js
@@ -4,12 +4,19 @@ var models = require('../../../models/');
 var router = express.Router();
 const request = require('request');
 
-router.get('/:idx', function(req, res) {
+router.post('/:idx', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     models.contents.update({
         cnt: Sequelize.literal("cnt + 1")}, {
         where: {idx: req.params.idx}
-    }).then(res.json({'check': true}));
+    }).then(function(result) {
+        models.archive.create({
+            userid: req.body.userid,
+            content_idx: req.params.idx
+        }).then(function(result) {
+            res.json({"check": true});
+        });
+    });
 });
 
 module.exports = router;
